5c99242bd4c79a929b3324224a56b1ac9ce3f44e
huaifeng <hfzhang.justin@gmail.com>
2024-12-13 21:45:55 +0100

    Update build script

--- pouw_docker/miner/amd64/entrypoint.sh
+++ pouw_docker/miner/amd64/entrypoint.sh
@@ -11,7 +11,7 @@ fi
 geth --allow-insecure-unlock --networkid=6666 --http --http.port=8545 \
   --http.addr=0.0.0.0 --http.api=eth,web3,net,admin,personal --http.corsdomain=* \
   --syncmode=full --nodekey nodekey \
-  --bootnodes="enode://c8dd7e533969999eb7626b1aaeebddc305bc7ba614b6bce1177e54cc9997432ae2c03ed9b8fe5dcd181966c1eafff612daa88875e8744f0df81f5c2a1c54b849@13.50.117.77:30303" \
+  --bootnodes="enode://9d2d0d7b8636b391ebd8a4b0f1450149fd088dd2d9f87ee3f3103e3ee159eb7981c82d1f680f1d6e5b6a9c92e3c317f48c40088a92634a866f599027b40d262d@13.50.117.77:30303" \
   --mine \
   --miner.threads=1 \
   --miner.etherbase=${MINER_ADDRESS}
--- pouw_docker/miner/arm64/entrypoint.sh
+++ pouw_docker/miner/arm64/entrypoint.sh
@@ -11,7 +11,7 @@ fi
 geth --allow-insecure-unlock --networkid=6666 --http --http.port=8545 \
   --http.addr=0.0.0.0 --http.api=eth,web3,net,admin,personal --http.corsdomain=* \
   --syncmode=full --nodekey nodekey \
-  --bootnodes="enode://5285ae5747310893d3d111ab19c46f8031ab584757bdf970cfa0957e98c118e37cc982aa6219b878ae2d07432924f1ed9276307422cc8768d43aef1fdc033445@13.50.117.77:30303" \
+  --bootnodes="enode://9d2d0d7b8636b391ebd8a4b0f1450149fd088dd2d9f87ee3f3103e3ee159eb7981c82d1f680f1d6e5b6a9c92e3c317f48c40088a92634a866f599027b40d262d@13.50.117.77:30303" \
   --mine \
   --miner.threads=1 \
   --miner.etherbase=${MINER_ADDRESS}
